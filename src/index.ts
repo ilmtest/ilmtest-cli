959bb50386c8685b4f350d99b4c058e37a64ff13
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-02-24 16:39:33 -0500

    feat(asl): Support uploading and downloading asl #2 (#3)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { select } from '@inquirer/prompts';
 import welcome from 'cli-welcome';
 
 import packageJson from '../package.json' assert { type: 'json' };
-import { transcribeWithAI } from './actions/transcribe.js';
 import { loadConfiguration } from './utils/config.js';
 
 const main = async () => {
@@ -15,16 +14,27 @@ const main = async () => {
         version: packageJson.version,
     });
 
-    await loadConfiguration(packageJson.name, ['collectionsEndpoint', 'tafrighApiKeys']);
-
     const action = await select({
-        choices: [{ name: 'AI Transcribe', value: 'transcribe' }],
+        choices: [
+            { name: 'AI Transcribe', value: 'transcribe' },
+            { name: 'Download Asl', value: 'downloadAsl' },
+            { name: 'Upload Asl', value: 'uploadAsl' },
+        ],
         default: 'transcribe',
         message: 'Select language',
     });
 
     if (action === 'transcribe') {
-        await transcribeWithAI();
+        await loadConfiguration(packageJson.name, ['collectionsEndpoint', 'tafrighApiKeys']);
+        await (await import('./actions/transcribe.js')).transcribeWithAI();
+    } else if (action === 'downloadAsl' || action === 'uploadAsl') {
+        await loadConfiguration(packageJson.name, ['awsRegion', 'awsAccessKey', 'awsSecretKey', 'awsBucket']);
+
+        if (action === 'downloadAsl') {
+            await (await import('./actions/downloadAsl.js')).downloadAsl();
+        } else if (action === 'uploadAsl') {
+            await (await import('./actions/uploadAsl.js')).uploadAsl();
+        }
     }
 };
 
