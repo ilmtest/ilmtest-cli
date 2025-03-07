b99ee01bfbb9f8f0de3f7181a8793166938ff3be
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-03-07 10:46:09 -0500

    feat(gzip): Add gzip compression to uploaded files

--- src/actions/uploadAsl.ts
+++ src/actions/uploadAsl.ts
@@ -1,33 +1,49 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { confirm, input } from '@inquirer/prompts';
 import { promises as fs } from 'node:fs';
+import path from 'node:path';
 
 import config from '../utils/config.js';
+import { compressFile } from '../utils/io.js';
 import logger from '../utils/logger.js';
 
+const sanitizeInput = (input: string) => input.replace(/\\ /g, ' ').trim();
+
 export const uploadAsl = async () => {
+    const filePath = sanitizeInput(
+        await input({
+            message: 'Enter the path to the JSON file:',
+            required: true,
+            transformer: (val) => sanitizeInput(val),
+            validate: async (input) => {
+                const file = sanitizeInput(input);
+
+                if (!file.endsWith('.json')) {
+                    return 'The asl must be a .json file';
+                }
+
+                try {
+                    await fs.access(file);
+                    return true;
+                } catch {
+                    return 'File does not exist. Please enter a valid file path';
+                }
+            },
+        }),
+    );
+
     const collectionId = await input({
+        default: path.parse(filePath).name,
         message: 'Enter collection ID to upload:',
         required: true,
         validate: (id) => (/\d+/.test(id) ? true : 'Please enter a valid collection ID'),
     });
 
-    const filePath = await input({
-        message: 'Enter the path to the JSON file:',
-        required: true,
-        validate: async (file) => {
-            if (!file.endsWith('.json')) {
-                return 'The asl must be a .json file';
-            }
+    logger.info(`Compressing ${filePath}`);
 
-            try {
-                await fs.access(file);
-                return true;
-            } catch {
-                return 'File does not exist. Please enter a valid file path';
-            }
-        },
-    });
+    const gzippedFilePath = await compressFile(filePath);
+
+    logger.info(`Successfully compressed file to ${gzippedFilePath}`);
 
     const s3Client = new S3Client({
         credentials: {
@@ -37,13 +53,13 @@ export const uploadAsl = async () => {
         region: config.awsRegion,
     });
 
-    const fileName = `${collectionId}.json`;
+    const fileName = `${collectionId}.json.gz`;
 
     logger.info(`Uploading ${fileName} to ${config.awsBucket}`);
 
     await s3Client.send(
         new PutObjectCommand({
-            Body: await fs.readFile(filePath),
+            Body: await fs.readFile(gzippedFilePath),
             Bucket: config.awsBucket,
             ContentType: 'application/json',
             Key: fileName,
@@ -53,6 +69,8 @@ export const uploadAsl = async () => {
     s3Client.destroy();
 
     logger.info(`Upload successful: ${fileName}`);
+
+    await fs.rm(gzippedFilePath, { recursive: true });
 
     const deleteFile = await confirm({ message: `Do we you want to delete ${filePath}` });
 
