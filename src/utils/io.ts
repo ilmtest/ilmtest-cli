a97f0588aa7aa084df2103cd5e32983be47736aa
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-04-05 01:54:26 -0400

    feat(v1.4): Switch to bun, improve download speeds (#9)
    * Switch over to using bun's S3 client #4
    
    * Update imports
    
    * Pick the fastest download instead of the first successful #8
    
    - Remove eslint import.
    - Add vitest eslint plugin.
    - Use newer eslint config.
    
    * Before querying baheth, check for local files already downloaded

--- src/utils/io.ts
+++ src/utils/io.ts
@@ -1,19 +1,8 @@
+import type { Readable } from 'node:stream';
+
 import { promises as fs } from 'node:fs';
-import { Readable } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
-import { createGunzip, createGzip } from 'node:zlib';
-
-export const compressFile = async (filePath: string, outputFilePath: string) => {
-    const sourceStream = await fs.open(filePath, 'r');
-    const destinationStream = await fs.open(outputFilePath, 'w');
-
-    await pipeline(sourceStream.createReadStream(), createGzip(), destinationStream.createWriteStream());
-
-    await sourceStream.close();
-    await destinationStream.close();
-
-    return outputFilePath;
-};
+import { createGunzip } from 'node:zlib';
 
 /**
  * Decompresses a gzipped stream and saves it to a file
